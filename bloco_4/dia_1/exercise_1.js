28461c554562c5cac07b686b7c4cfafc8274363a
Diego Vilela <vilelaphp@gmail.com>
2021-05-20 09:24:20 -0300

    Exercício 1

---
+++ bloco_4/dia_1/exercise_1.js
@@ -0,0 +1,23 @@
+let a = 7,
+    b = 3;
+
+function adicao(n1, n2) {
+    return n1 + n2;
+}
+
+function subtracao(n1, n2) {
+    return n1 - n2;
+}
+
+function multiplicacao(n1, n2) {
+    return n1 * n2;
+}
+
+function modulo(n1, n2) {
+    return n1 % n2;
+}
+
+console.log('Adição: ', adicao(a, b));
+console.log('Subtração: ', subtracao(a, b));
+console.log('Multiplicação: ', multiplicacao(a, b));
+console.log('Módulo: ', modulo(a, b));
