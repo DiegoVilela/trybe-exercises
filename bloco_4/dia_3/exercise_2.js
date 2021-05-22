599fdf1ead233ac83e1e9e4c4ce34f8e1b5baa83
Diego Vilela <vilelaphp@gmail.com>
2021-05-22 14:12:31 -0300

    Exercise 2

---
+++ bloco_4/dia_3/exercise_2.js
@@ -0,0 +1,9 @@
+/*
+Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+*/
+
+const base = 5;
+
+for (let i = 1; i <= base; i++) {
+    console.log('*'.repeat(i));
+}
