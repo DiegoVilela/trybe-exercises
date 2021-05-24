a0726ab3aa7a1d9dfdd5cac7b7c572371b213170
Diego Vilela <vilelaphp@gmail.com>
2021-05-24 17:42:40 -0300

    Part 2 exercise 6

--- dia_4/part_2_exercises.js
+++ dia_4/part_2_exercises.js
@@ -77,3 +77,17 @@ function mostCommon(intArray) {
 
     return ints[key];
 }
+
+/*
+6. Crie uma função que receba um número inteiro N e
+retorne o somatório de todos os números de 1 até N.
+*/
+function sumAllTill(int) {
+    let total = 0;
+
+    for (let i = 1; i <= int; i++) {
+        total += i;
+    }
+
+    return total;
+}
