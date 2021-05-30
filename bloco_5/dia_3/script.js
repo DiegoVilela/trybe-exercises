5bbb75ec99313e2d5dedc1f1b5b73a6bb52d54a2
Diego Vilela <vilelaphp@gmail.com>
2021-05-30 13:42:28 -0300

    Exercise 3

--- bloco_5/dia_3/script.js
+++ bloco_5/dia_3/script.js
@@ -2,6 +2,7 @@ window.onload = () => {
   createDaysOfTheWeek();
   createMonth(dezDaysList, holidays, fridays);
   addButtonFeriadosToContainer();
+  addEventListenerToFeriadosBtn();
 }
 
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
@@ -43,13 +44,36 @@ function createMonth(days, holidays, fridays) {
 /** Exercise 2 */
 
 function createButton(str) {
-  let btn = document.createElement('button');
+  const btn = document.createElement('button');
   btn.innerText = str;
-  btn.className = 'btn-holidays';
+  btn.id = 'btn-holidays';
   return btn;
 }
 
 function addButtonFeriadosToContainer() {
-  let btnContainer = document.getElementsByClassName('buttons-container')[0];
+  const btnContainer = document.getElementsByClassName('buttons-container')[0];
   btnContainer.appendChild(createButton('Feriados'));
+}
+
+
+/** Exercise 3 */
+
+function addEventListenerToFeriadosBtn() {
+  const btn = document.getElementById('btn-holidays');
+  const holidays = document.getElementsByClassName('holiday');
+
+  btn.addEventListener('click', () => {
+    let bgColor = 'green';
+    const currentBgColor = holidays[0].style.backgroundColor;
+
+    for (let day of holidays) {
+      if (currentBgColor) {
+        day.style.removeProperty('background-color');
+        day.style.removeProperty('color');
+      } else {
+        day.style.backgroundColor = bgColor;
+        day.style.color = 'white';
+      }
+    }
+  });
 }
