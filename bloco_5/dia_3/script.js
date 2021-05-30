a1d2b29a1a601649a242fed91d7039011910fecb
Diego Vilela <vilelaphp@gmail.com>
2021-05-30 13:00:53 -0300

    Exercise 2

--- bloco_5/dia_3/script.js
+++ bloco_5/dia_3/script.js
@@ -1,6 +1,7 @@
 window.onload = () => {
   createDaysOfTheWeek();
   createMonth(dezDaysList, holidays, fridays);
+  addButtonFeriadosToContainer();
 }
 
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
@@ -36,4 +37,19 @@ function createMonth(days, holidays, fridays) {
     dayListItem.innerHTML = day;
     monthDaysList.appendChild(dayListItem);
   }
+}
+
+
+/** Exercise 2 */
+
+function createButton(str) {
+  let btn = document.createElement('button');
+  btn.innerText = str;
+  btn.className = 'btn-holidays';
+  return btn;
+}
+
+function addButtonFeriadosToContainer() {
+  let btnContainer = document.getElementsByClassName('buttons-container')[0];
+  btnContainer.appendChild(createButton('Feriados'));
 }
