a69ba3de20967925c2a3044c42e739582459c57b
Diego Vilela <vilelaphp@gmail.com>
2021-06-24 16:14:40 -0300

    8.1 Exercise 3

--- 8-higher-order-functions-do-javascript-es6/dia_1/exercises.js
+++ 8-higher-order-functions-do-javascript-es6/dia_1/exercises.js
@@ -25,3 +25,30 @@ const betResult = (betNumber, betChecker) => {
 }
 
 console.log(betResult(3, wonBet));
+
+
+/** Exercise 3 */
+/*
+Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+
+    Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+*/
+
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const pointsChecker = (target, attempt) => {
+  let points = 0;
+  for (let i = 0; i <= target.length; i += 1) {
+    if (attempt[i] === 'N.A') continue;
+
+    (target[i] === attempt[i]) ? points += 1 : points -= 0.5;
+  }
+  return points;
+}
+
+const assessmentHandler = (template, answers, checker) => {
+  return checker(template, answers);
+}
+
+console.log(assessmentHandler(rightAnswers, studentAnswers, pointsChecker));
