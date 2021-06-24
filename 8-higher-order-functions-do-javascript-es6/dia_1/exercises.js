3692385497e1fb49b3ad0b9ee9085f20e27161b8
Diego Vilela <vilelaphp@gmail.com>
2021-06-24 15:30:54 -0300

    8.1 Exercise 2

--- 8-higher-order-functions-do-javascript-es6/dia_1/exercises.js
+++ 8-higher-order-functions-do-javascript-es6/dia_1/exercises.js
@@ -10,7 +10,18 @@ const newEmployees = (chosenFunc) => {
 };
 
 const slugfy = (str) => str.toLocaleLowerCase().replace(/ /g, '_');
-
 const createEmployee = (fullName) => ({fullName, email: `${slugfy(fullName)}@trybe.com`});
 
 console.log(newEmployees(createEmployee));
+
+
+/** Exercise 2 */
+
+const wonBet = (expected, actual) => expected === actual;
+
+const betResult = (betNumber, betChecker) => {
+  const randomNumber = Math.floor(Math.random() * 5) + 1;
+  return (wonBet(betNumber, randomNumber)) ? 'Parabéns você ganhou' : 'Tente novamente';
+}
+
+console.log(betResult(3, wonBet));
