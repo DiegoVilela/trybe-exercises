6d5a48c9cd0edff7552ad41acea6237f608b0f06
Diego Vilela <vilelaphp@gmail.com>
2021-06-29 13:54:42 -0300

    8.4 Exercise 6

---
+++ 8-higher-order-functions-do-javascript-es6/dia_4/exercise6.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const studentAverage = () => students.map(
+  (student, idx) => (
+    {
+      name: student,
+      average: grades[idx].reduce(
+        (total, grade) => total + grade) / grades[0].length
+    }
+  ));
+
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+assert.deepStrictEqual(studentAverage(), expected);
