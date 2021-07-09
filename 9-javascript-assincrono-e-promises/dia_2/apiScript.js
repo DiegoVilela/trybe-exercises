094c66bbfb8b6267ac33180ae6c6f1dbb7bc7b2e
Diego Vilela <vilelaphp@gmail.com>
2021-07-09 11:05:04 -0300

    Implement requirement 4 and 5

--- 9-javascript-assincrono-e-promises/dia_2/apiScript.js
+++ 9-javascript-assincrono-e-promises/dia_2/apiScript.js
@@ -22,7 +22,7 @@ const myPromise = new Promise((resolve, reject) => {
   (arraySum < 8000) ? resolve(arraySum) : reject(new Error('Sum >= 8000'));
 })
 .then((response) => [2, 3, 5, 10].map((i) => response / i))
-.then((resultArray) => console.log(resultArray))
-.catch(() => console.log('Promise rejeitada'));
+.then((resultArray) => console.log(resultArray.reduce((total, curr) => total + curr)))
+.catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
 
 window.onload = () => fetchJoke();
