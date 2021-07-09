4780c42aaefa82d10ed365021bb999919700df7a
Diego Vilela <vilelaphp@gmail.com>
2021-07-09 12:09:44 -0300

    Bonus

--- 9-javascript-assincrono-e-promises/dia_2/exercises3to5.js
+++ 9-javascript-assincrono-e-promises/dia_2/exercises3to5.js
@@ -1,12 +1,27 @@
-const myPromise = new Promise((resolve, reject) => {
-  const array = Array.from(
-    { length: 10 },
-    () => ((Math.random() * 51) + 1) ** 2
-  );
-  const arraySum = array.reduce((acc, curr) => acc + curr);
-  (arraySum < 8000) ? resolve(arraySum) : reject(new Error('Sum >= 8000'));
-})
-.then((response) => [2, 3, 5, 10].map((i) => response / i))
-.then((resultArray) => console.log(resultArray.reduce((total, curr) => total + curr)))
-.catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
+const createArryOfRamdonNumbers = () => Array.from(
+  { length: 10 },
+  () => ((Math.random() * 51) + 1) ** 2
+);
 
+const arraySum = (array) => {
+  const sum = array.reduce((acc, curr) => acc + curr);
+  if (sum < 8000) return sum;
+
+  throw new Error();
+};
+
+const arrayOfResults = (arraySum) => [2, 3, 5, 10].map((i) => arraySum / i);
+
+const getSumOfRandomNumbers = async () => {
+  try {
+    const arrayOfRandom = await createArryOfRamdonNumbers();
+    const sum = await arraySum(arrayOfRandom);
+    const arrayResult = await arrayOfResults(sum);
+    const finalSum = await arrayResult.reduce((acc, curr) => acc + curr);
+    console.log(finalSum);
+  } catch (error) {
+    console.log('É mais de oito mil! Essa promise deve estar quebrada!');
+  }
+};
+
+getSumOfRandomNumbers();
