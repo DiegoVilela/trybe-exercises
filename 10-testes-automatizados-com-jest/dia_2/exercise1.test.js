4c9c006c77704dc2186a9384430d5eaaf049f8fe
Diego Vilela <vilelaphp@gmail.com>
2021-07-16 14:40:01 -0300

    Remove console log

--- 10-testes-automatizados-com-jest/dia_2/exercise1.test.js
+++ 10-testes-automatizados-com-jest/dia_2/exercise1.test.js
@@ -2,8 +2,6 @@ const uppercase = (str, callback) => {
   callback(str.toUpperCase());
 };
 
-// uppercase('oi', console.log);
-
 test('it returns the uppercase version of the string', done => {
   uppercase('go up', (str) => {
     expect(str).toBe('GO UP');
