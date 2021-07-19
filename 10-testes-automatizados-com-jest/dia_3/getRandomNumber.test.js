344bb994146bdb7480a388632cff03199d170e39
Diego Vilela <vilelaphp@gmail.com>
2021-07-19 14:40:26 -0300

    10.3 Exercise 3

--- 10-testes-automatizados-com-jest/dia_3/getRandomNumber.test.js
+++ 10-testes-automatizados-com-jest/dia_3/getRandomNumber.test.js
@@ -1,11 +1,10 @@
 const { getRandomNumber } = require('./main');
 
 jest.mock('./main');
+getRandomNumber.mockReturnValue(10);
 
 describe('Testing getRandomNumber', () => {
   it('should return a random number', () => {
-    getRandomNumber.mockReturnValue(10);
-
     expect(getRandomNumber()).toBe(10);
     expect(getRandomNumber).toHaveBeenCalled();
     expect(getRandomNumber).toHaveBeenCalledTimes(1);
@@ -18,5 +17,24 @@ describe('Testing getRandomNumber', () => {
     expect(getRandomNumber).toHaveBeenCalledTimes(2);
     expect(getRandomNumber()).toBe(10);
     expect(getRandomNumber).toHaveBeenCalledTimes(3);
+  });
+
+  it('should return the multiplication of three numbers', () => {
+    getRandomNumber.mockImplementationOnce((a, b, c) => a * b * c);
+
+    expect(getRandomNumber(2, 3, 4)).toBe(24);
+    expect(getRandomNumber).toBeCalledWith(2, 3, 4);
+    expect(getRandomNumber).toBeCalledTimes(4);
+    expect(getRandomNumber()).toBe(10);
+    expect(getRandomNumber).toBeCalledTimes(5);
+  });
+
+  it('should return the double of a number', () => {
+    getRandomNumber.mockImplementationOnce((n) => n * 2);
+
+    expect(getRandomNumber(3)).toBe(6);
+    expect(getRandomNumber).toBeCalledWith(3);
+    expect(getRandomNumber).toBeCalledTimes(6);
+    expect(getRandomNumber()).toBe(10);
   });
 });
