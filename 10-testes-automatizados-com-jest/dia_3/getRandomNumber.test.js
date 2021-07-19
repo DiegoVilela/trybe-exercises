c8f070b936c9e04943628b9ed762bc62582c967e
Diego Vilela <vilelaphp@gmail.com>
2021-07-19 12:01:03 -0300

    10.3 Exercise 2

--- 10-testes-automatizados-com-jest/dia_3/getRandomNumber.test.js
+++ 10-testes-automatizados-com-jest/dia_3/getRandomNumber.test.js
@@ -10,4 +10,13 @@ describe('Testing getRandomNumber', () => {
     expect(getRandomNumber).toHaveBeenCalled();
     expect(getRandomNumber).toHaveBeenCalledTimes(1);
   });
+
+  it('should return the division of to numbers', () => {
+    getRandomNumber.mockImplementationOnce((a, b) => a / b);
+
+    expect(getRandomNumber(10, 2)).toBe(5);
+    expect(getRandomNumber).toHaveBeenCalledTimes(2);
+    expect(getRandomNumber()).toBe(10);
+    expect(getRandomNumber).toHaveBeenCalledTimes(3);
+  });
 });
